cae899fb6b3f892c2801c31c0f5b2fb27f7b3949
Bianca <biancavmoraes@outlook.com>
2024-06-14 14:59:44 -0300

    Iniciando implementação de busca por usuários

--- Seção 17: PHP & MySQL/Twitter/procurar_pessoas.php
+++ Seção 17: PHP & MySQL/Twitter/procurar_pessoas.php
@@ -88,7 +88,7 @@
 			<div class="col-md-3">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <h4><a href="procurar_pessoas.php">Procurar por pessoas</a></h4>
+                        <h4><a href="#">Procurar por pessoas</a></h4>
                     </div>
                 </div>
             </div>
